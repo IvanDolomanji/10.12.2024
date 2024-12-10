bc77b8531f76d69842cf7f00d8fdbfe51f4c4182
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2024-12-10 13:45:14 +0200

    Add files via upload

---
+++ dolomanji/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dolomanji
+{
+    /// <summary>
+    /// отсортировать массив по возрастанию и убыванию
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice" };
+            Array.Sort(people);
+            foreach (var el in people)
+            {
+                Console.WriteLine(el);
+            }
+            Array.Reverse(people);
+            foreach(var el in people)
+            {
+                Console.WriteLine(el);
+            }
+            Console.ReadKey();
+        }
+    }
+}
