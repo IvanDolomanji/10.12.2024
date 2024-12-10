85100174e0c85a056ece7eccad3c3dfe11abf605
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2024-12-10 14:07:30 +0200

    Add files via upload

---
+++ dolomanji2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dolomanji2
+{
+    /// <summary>
+    /// подсчитать количество числа со значением 5 в одномерном массиве
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = { 5, 7, 1, 9, 5, 3, 6, 5, 4 };
+            //int count = 0;
+            //foreach(var el in omas)
+            //{
+            //    if (el == 5)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+            //способ 2
+            int count = omas.Count(x => x == 5);
+            Console.WriteLine(count);
+            Console.Read();
+        }
+    }
+}
