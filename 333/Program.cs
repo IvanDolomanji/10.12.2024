73d5658790f16f4bf7e3ff209b2b090bf1480d54
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2024-12-10 14:24:01 +0200

    Add files via upload

---
+++ 333/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _333
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = { 5, 7, 1, 9, 5, 3, 6, 5, 4 };
+            omas = omas.Where(x => x % 2 != 0).ToArray();
+            foreach (var el in omas)
+            {
+                Console.Write($"{el}\t");
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
